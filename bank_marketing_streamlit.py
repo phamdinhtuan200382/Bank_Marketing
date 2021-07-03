e0c7976312db5cfdf2f9b71035525cb332b161b9
Tuan Pham Dinh <dinhtuan3955@gmail.com>
2021-07-03 21:39:08 +0700

    update all file

--- bank_marketing_streamlit.py
+++ bank_marketing_streamlit.py
@@ -1,28 +1,192 @@
-import numpy as np
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+from sklearn.preprocessing import MinMaxScaler
+import json
+import pickle
+from matplotlib import pyplot as plt
 import seaborn as sns
+pd.plotting.register_matplotlib_converters()
 import warnings
 warnings.filterwarnings("ignore")
-import streamlit as st
-import pickle
-import sys
-from sklearn.preprocessing import OrdinalEncoder
-encoder=OrdinalEncoder()
-        
-from sklearn.utils import resample
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score,classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import precision_recall_curve
+from sklearn import metrics
+from xgboost import XGBClassifier
+#import scikitplot as skplt
+import matplotlib.pyplot as plt
 import base64
 
-main_bg = "D://photo.jfif"
-main_bg_ext = "jfif"
+def transform_pdays(val):
+    transform_dict = {999:'not_previously_contacted',7: 'over_a_week',0:'within_a_week'}
+    for key in transform_dict.keys():
+        if (val >= key):
+            return transform_dict[key]
+        
+### processing input data
+def process_input_client(client_df):
+    num_cols = marketing_df.dtypes[marketing_df.dtypes != 'object'].index.tolist()
+    cat_cols = [col for col in marketing_df.dtypes[marketing_df.dtypes == 'object'].index.tolist() if col != 'y']
+
+    missing_val = 'unknown'
+    un_replaced_lst = ['default']
+    for col in cat_cols:
+        if (col in un_replaced_lst):
+            continue
+        replaced_val = marketing_df[col].mode().values.tolist()[0]
+        client_df[col] = client_df[col].apply(lambda val: replaced_val if val == missing_val else val)
 
 
+    for col in num_cols:
+        if col == 'pdays':
+            client_df[col] = client_df[col].map(transform_pdays)
+            cat_cols = cat_cols +['pdays'] 
+            continue
 
-st.markdown(
+        if col == 'campaign':
+            replaced_val = 6
+        else:
+            replaced_val = marketing_df[col].quantile(0.95)   
+
+        client_df[col] = client_df[col].apply(lambda val: replaced_val if val > replaced_val else val)
+
+    ## onehot_encoding:
+    cat_cols = [col for col in client_df.dtypes[client_df.dtypes == 'object'].index.tolist()]  
+    labelencoder = LabelEncoder()
+    for column in cat_cols:
+        client_df[column] = labelencoder.fit_transform(client_df[column])
+        
+    return(client_df)  
+ 
+def get_cat_cols_val(data):
+    unique_dict= {}
+    cat_cols = data.dtypes[data.dtypes == 'object'].index
+    for col in cat_cols:
+        unique_col = data[col].unique()
+        unique_dict[col] = unique_col
+    return unique_dict
+
+def quick_predict_client():
+    client_df_ok = pd.read_csv("data/X_client_df.csv", index_col = 'Unnamed: 0')
+
+    target = 'y'
+    tam = 1
+    # num_cols = marketing_df.dtypes[marketing_df.dtypes != 'object'].index.tolist()
+    # cat_cols = [col for col in marketing_df.dtypes[marketing_df.dtypes == 'object'].index.tolist() if col != target]
+    cols = [col for col in marketing_df.columns.tolist() if col != 'y']
+    col_types = [marketing_df[col].dtype.name for col in cols]
+    client_df = pd.DataFrame()
+    question_dict = {"age": "Enter age of the customer", "job": "Enter the type of job customer do","marital": "what is customer's marital status?", "education":"Enter customer's education level?",
+    "default": "Do the customer have credit in default?", "housing": "Do the customer have Housing Loan?", "loan": "Do the customer have Personal Loan?", "contact": "How do you prefer to communicate",
+    "month": "Which month the customer was last contacted in?", "day_of_week": "Which day of week the customer was contacted in", "duration": "Enter last contact duration with the customer in sec?",
+    "campaign": "Enter number of contacts performed during this campaign and for this client", "pdays": "Enter number of days that passed by after the client was last contacted from a previous campaign",
+    "previous": "Enter number of contacts performed before this campaign and for this client", "poutcome": "Enter outcome of the previous marketing campaign", "emp.var.rate": "Enter employment variation rate - quarterly indicator",
+    "cons.price.idx": "Enter consumer price index - monthly indicator", "cons.conf.idx": "Enter consumer confidence index - monthly indicator", "euribor3m": "Enter euribor 3 month rate - daily indicator", "nr.employed": "Enter number of employees - quarterly indicator"}
+    for col, col_dtype, key in zip(cols,col_types, question_dict):
+        if (col_dtype == 'object'):
+            col_option_lst = marketing_df[col].unique().tolist()
+            col_mode = marketing_df[col].mode().tolist()[0]
+            
+            col_selected = col_option_lst.index(client_df_ok.iloc[tam][col])
+            col_option= st.selectbox(question_dict[key],options = col_option_lst, index = col_selected)  
+            client_df[col] = [col_option]
+        else:
+            min_val  = int(marketing_df[col].min())
+            max_val  = int(marketing_df[col].max())
+            value =  int(client_df_ok.iloc[tam][col])
+            col_option = st.slider(question_dict[key], min_value= min_val, max_value= max_val, value=value, step=1)
+            
+            client_df[col] = [col_option]
+
+    if st.button('Predict'):
+            st.write(client_df)
+            #client_df = client_df_ok .drop(['y'], axis = 1) 
+            client_df = process_input_client(client_df)
+            X_test = scaler.transform(client_df)
+            
+            y_pred = model.predict(X_test)
+            result_str = 'high - potential' if y_pred == 1 else 'low - potential'
+            result ='This is a '+ result_str + ' client for this tele marketing campain'
+            st.success(result)
+
+def visualize_predicted_result(df, target):
+    data = df.groupby(target).size().sort_values(ascending=False)
+    label_dict = {1:'yes',0:'no'}
+    fig = plt.figure(figsize = (4,3))
+    plt.pie(x=data , autopct="%.1f%%", explode=[0.05]*len(data), labels= [label_dict[val] for val in data.index.tolist()]);
+    plt.title("The predicted percentage of success");
+    st.pyplot(fig)  
+    
+def predict_data_file(file):
+    upload_data = get_df(file)
+    features = [col for col in marketing_df.columns.tolist() if col != 'y']
+    input_data  = upload_data[features]
+    st.markdown('Lis of clients to predict:')
+    st.write(input_data)
+    
+    ## prediction
+    client_df = process_input_client(input_data)
+    X_client_test = scaler.transform(client_df)
+    y_client_pred = model.predict(X_client_test)
+    result_col = 'predict'
+    upload_data[result_col] = y_client_pred
+    
+    ## summary result  
+    pred_success_cnt = sum((y_client_pred == 1))
+    total_cnt = len(y_client_pred)
+    st.markdown('Predicted result:')
+    st.markdown (str(pred_success_cnt) +" clients will say YES over "+str(total_cnt) +' people')
+    visualize_predicted_result(upload_data, result_col)
+    
+    ## view result
+    view_result_option = ['view all',"view successful list", "view unsuccesful list"]
+    view_type_id = st.selectbox('Choose view:',options = view_result_option)
+    if (view_type_id == view_result_option[0]):
+        st.write(upload_data) 
+    else:
+        if (view_type_id == view_result_option[1]):
+            view_filter = upload_data[result_col] == 1 
+        else:
+            view_filter = upload_data[result_col] == 0
+        
+        st.write(upload_data[view_filter])
+     
+
+     
+        
+# Initial setup
+# st.set_page_config(layout="wide")
+def get_df(file):
+      # get extension and read file
+  extension = file.name.split('.')[1]
+  if extension.upper() == 'CSV':
+    df = pd.read_csv(file,sep = ';')
+  elif extension.upper() == 'XLSX':
+    df = pd.read_excel(file, engine='openpyxl')
+  elif extension.upper() == 'PICKLE':
+    df = pd.read_pickle(file)
+  return df
+
+#### L O A D  Data
+data_file_path = "data/bank-additional-full.csv"
+marketing_df = pd.read_csv(data_file_path,sep = ";")
+
+model_file_path = "model/pkl_model.pkl"
+model = pickle.load(open(model_file_path, 'rb'))
+
+scaler_file_path = "model/pkl_scaler.pkl"
+scaler = pickle.load(open(scaler_file_path, 'rb'))
+
+# Main function
+def main():
+    main_bg = "image/photo.jfif"
+    main_bg_ext = "jfif"
+    st.markdown(
     f"""
     <style>
     .reportview-container {{
@@ -31,11 +195,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-print(sys.path)
-data=pd.read_csv("D:\\bank-additional-full.csv", sep = ";")
-def main():
-    
     st.title("Bank Marketing Prediction")
     
     htk=  """
@@ -44,135 +203,18 @@ def main():
     </div>
     """
     st.markdown(htk,unsafe_allow_html=True)
+    predict_option = ['Quick Predict','Predict With Data File']
+    predict_type_id = st.sidebar.selectbox('CHOOSE PREDICT:',options = predict_option)
     
-    if st.button("Problem Statement"):
-       
-        st.markdown(""" The  goal is to make a predictive model to predict if the customer will respond positively to the
-            campaign organised by a portugese bank institution. Often, more than one contact to the same client was required, in order to access 
-        if the product (bank term deposit) would be subscribed or not subscribed.
-        This is a tedious task to do and will consume much time ,the model can help in avoiding both.
-         """)
-    if st.checkbox("Data Description"):
-        st.text(
-        """ 
-        The data is related with direct marketing campaigns of a Portuguese banking institution.
-        The marketing campaigns were based on phone calls.All the information can be found in the link given below.
-        Please refer to the data set link before actually going forward with the app to get a better understanding and clear idea.
-        """)
-          
-        link='[Data set link](https://archive.ics.uci.edu/ml/datasets/Bank+Marketing)'
-        st.markdown(link,unsafe_allow_html=True)
-
-
-    st.sidebar.header("GRADIENT BOOST CLASSIFIER")   
-    age=st.slider("Enter age of the customer",18,95)
-    age=int(age)
-    # age=int(scaler.fit_transform([[age]]))
-
-    job=st.selectbox("Enter the type of job customer do",("select",'housemaid', 'services', 'admin.', 'blue-collar', 'technician',
-    'retired', 'management', 'unemployed', 'self-employed', 'unknown', 'entrepreneur', 'student'))
-    job=int(encoder.fit_transform([[job]]))
-        
-    marital=st.selectbox("what is customer's marital status?",("select",'married','single' ,'divorced'))
-    
-    education=st.selectbox("Enter customer's education level",("select",'basic.4y', 'basic.6y' ,'basic.9y' ,'high.school','professional.course','university.degree','unknown','illiterate'))
-    
-    #targeted=st.selectbox("Do the customer have target?", ("select",'yes' , 'no'))
-    
-    default=st.selectbox("Do the customer have credit in default?", ("select",'yes' , 'no'))
-            
-    housing=st.selectbox("Do the customer have Housing Loan?", ("select",'yes' , 'no'))
-    
-    loan=st.selectbox("Do the customer have Personal Loan?",("select",'yes', 'no'))
-    
-    contact=st.radio("How do you prefer to communicate",("select","unknown",'telephone', 'cellular'))
-
-    month=st.selectbox("Which month the customer was last contacted in?",("select",'jan',"feb","mar",'apr',"may","jun","jul","aug","sep","oct","nov","dec"))
-    
-    day_of_week=st.radio("Which day of week the customer was contacted in",("select","mon",'tue', 'wed','thu','fri'))
-
-    duration=st.text_input("Enter last contact duration with the customer in sec?",0,4918)
-    if not duration:
-        st.warning("Enter Duration Period")
-        
-    else:
-        if duration.isalpha() and duration.isalnum():
-            st.warning("Please enter an integer number")
-            pass
+    if (predict_type_id  == predict_option[0]):
+        quick_predict_client()
+    elif (predict_type_id  == predict_option[1]):
+        file = st.file_uploader("Upload file", type=['csv'])
+        if not file:
+            st.write("Upload a .csv or .xlsx file to get started")
         else:
-            duration=int(duration)
-            if duration>4918 or duration<0:
-                st.warning("Please enter an number between 0 & 4918")
-            # duration=int(scaler.fit_transform([[duration]]))
+            predict_data_file(file)
+           
+main()
 
-
-    campaign=st.slider("Enter number of contacts performed during this campaign and for this client",1,63)
-    campaign=int(campaign)
-    # campaign=int(scaler.fit_transform([[campaign]]))
-
-    pdays=st.slider("Enter number of days that passed by after the client was last contacted from a previous campaign",0,27)
-    pdays=int(pdays)
-    # pdays=int(scaler.fit_transform([[pdays]]))
-
-    previous=st.slider("Enter number of contacts performed before this campaign and for this client",0,7)
-    previous=int(previous)
-    # previous=int(scaler.fit_transform([[previous]]))
-
-    poutcome=st.radio("Enter outcome of the previous marketing campaign",("select",'failure','nonexistent','success'))
-
-    emp_var_rate= int(1.100)
-
-    cons_price_idx = int(93.749)
-
-    cons_conf_idx = int(-41.8)
-
-    euribor3m = int(4.857)
-    
-
-    if marital!="select" and education!="select" and default!="select" and housing!="select" and loan!="select" and contact!="select" and month!="select" and day_of_week!="select" and duration!="sellect" and poutcome!="select":
-        marital=int(encoder.fit_transform([[marital]]))
-        education=int(encoder.fit_transform([[education]]))
-        default=int(encoder.fit_transform([[default]]))
-        housing=int(encoder.fit_transform([[housing]]))
-        loan=int(encoder.fit_transform([[loan]]))
-        contact=int(encoder.fit_transform([[contact]]))
-        month=int(encoder.fit_transform([[month]]))
-        day_of_week=int(encoder.fit_transform([[day_of_week]]))
-        poutcome=int(encoder.fit_transform([[poutcome]]))
-
-        with open("D:\\optimal_model.pkl",'rb') as f:
-            rf=pickle.load(f)
-        res=rf.predict([[age,job,marital,education,default,housing,loan,contact,month,duration,campaign,pdays,previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m]])
-        res=str(res)
-        dict={"yes":'1',"no":'0'}    
-        for i,j in dict.items():
-            res=res.replace(j,i)
-    else:
-        res="None"
-
-
-    
-
-if __name__=='__main__':
-    Res=main()
-    # Res=str(int(result))
-    # dict={"yes":'1',"no":'0'}    
-    # for i,j in dict.items():
-    #     Res=Res.replace(j,i)
-    
-            
-    if st.sidebar.button("Show Prediction"):
-        st.sidebar.subheader("The predicted response of customer or client to subscribe a term deposit is")
-        st.sidebar.success(Res)
-        st.sidebar.header('   ')
-        st.sidebar.image('https://media.giphy.com/media/jO2PObgv4R3JrSBFul/giphy.gif')
-
-
-        
-    
-    if st.button("Thanks") :
-        st.text("Thank you for visiting  and happy learning :)")
-        st.balloons()
-    
-   
-# streamlit run Bank_Marketing\bank_marketing_streamlit.py
+## Run: streamlit run bank_marketing_streamlit.py
